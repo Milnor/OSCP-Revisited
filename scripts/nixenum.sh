2d1cc055c58a5c6fe6be4a0b21c4dbfaaba26809
Seaver Milnor <b0rax@NaegleriaFowleri>
2022-07-02 18:41:40 -0400

    WIP

--- scripts/nixenum.sh
+++ scripts/nixenum.sh
@@ -17,7 +17,14 @@ KERNEL=$(uname -r)
 
 # Last field of ps with pid 1
 # shellcheck reports SC2009, but I doubt pgrep is a portable alternative
+# Could alternatively use `readlink -f /sbin/init` which is a symlink
+#	to systemd on a systemd system
 SVCMGR=$(ps -p 1 | grep -o '[a-z]*$')
+
+# /etc/timezone on Debian, /etc/localtime (not ASCII) on RedHat
+# TODO: fix this to support other distros
+TIMEZONE=$(cat /etc/timezone)
+# date +"%:z" might be more portable, but it's a number
 
 printf "[+] Architecture:\t %s\n" "$ARCH"
 printf "[+] CPU:\t\t %s\n" "$CPU"
@@ -26,3 +33,5 @@ printf "[+] OS:\t\t\t %s\n" "$OS"
 printf "[+] Kernel:\t\t %s\n" "$KERNEL"
 printf "[+] Service Manager:\t %s\n" "$SVCMGR"
 printf "[+] Shell:\t\t %s\n" "$SHELL"
+printf "[+] Language:\t\t %s\n" "$LANG"
+printf "[+] Timezone:\t\t %s\n" "$TIMEZONE"
